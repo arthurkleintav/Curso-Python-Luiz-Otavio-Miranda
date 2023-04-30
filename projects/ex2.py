c8184820490ba372f67798c77de8edce1714b928
Arthur Klein <arthurk0527@gmail.com>
2023-04-30 14:29:30 -0300

    Update ex2.py

--- projects/ex2.py
+++ projects/ex2.py
@@ -3,4 +3,5 @@ altura = 1.76
 peso = 60
 imc = peso / (altura * altura)
 
-print(nome, 'tem', altura, 'de altura,', 'pesa', peso, 'quilos', 'e seu IMC é', imc)
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos', 'e seu IMC é', imc)
