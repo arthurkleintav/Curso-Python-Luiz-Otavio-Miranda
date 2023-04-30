7dfc182eb1ae6f383e6649e78ca4a90ce106c945
Arthur Klein <arthurk0527@gmail.com>
2023-04-30 17:30:22 -0300

    fiz o terceiro exercício.

---
+++ projects/ex3.py
@@ -0,0 +1,7 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if int(primeiro_valor) > int(segundo_valor):
+    print(f'{primeiro_valor=} é maior do que {segundo_valor=}.')
+else:
+    print(f'{segundo_valor=} é maior do que {primeiro_valor=}')
