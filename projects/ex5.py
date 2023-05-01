b920c5c2e732a1d8d4f82b48e576f4eb8d5bb6ca
Arthur Klein <arthurk0527@gmail.com>
2023-05-01 13:01:34 -0300

    Terminei o exercício.

--- projects/ex5.py
+++ projects/ex5.py
@@ -20,19 +20,33 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
-print('-='*15)
+print('-=' * 15)
 
-hora = int(input('Que horas são? '))
-
-if 0 <= hora <= 11:
-    print('Bom dia!')
-elif 12 <= hora <= 17:
-    print('Boa tarde!')
-elif 18 <= hora <= 23:
-    print('Boa noite!')
+try:
+    hora = int(input('Que horas são? '))
+    if 0 <= hora <= 11:
+        print('Bom dia!')
+    elif 12 <= hora <= 17:
+        print('Boa tarde!')
+    elif 18 <= hora <= 23:
+        print('Boa noite!')
+except:
+    print('Por favor, digite um horário válido.')
 
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
+print('-=' * 15)
+
+primeiro_nome = input('Qual é o seu primeiro nome? ')
+
+tamanho_nome = len(primeiro_nome)
+
+if tamanho_nome <= 4:
+    print('Seu nome é curto.')
+elif 5 <= tamanho_nome <= 6:
+    print('Seu nome é normal.')
+else:
+    print('Seu nome é muito grande!')
