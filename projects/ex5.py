2a59e9af30eada9cf19615debb08610f06fa5ed2
Arthur Klein <arthurk0527@gmail.com>
2023-05-01 12:50:31 -0300

    Update ex5.py

--- projects/ex5.py
+++ projects/ex5.py
@@ -4,6 +4,16 @@ informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
 
+try:
+    num = int(input('Digite um número inteiro: '))
+    if num % 2 == 0:
+        print(f'O número {num} é par.')
+    else:
+        print(f'O número {num} é ímpar.')
+except:
+    print('O valor informado não é um número inteiro.')
+
+
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
