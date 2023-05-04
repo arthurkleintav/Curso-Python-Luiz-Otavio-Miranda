d05d84caffe7f81271c43043df6c97fa06484291
Arthur Klein <arthurk0527@gmail.com>
2023-05-04 15:47:07 -0300

    exercício 8

---
+++ projects/ex8.py
@@ -0,0 +1,25 @@
+frase = 'Python é muito bom'
+
+i = 0
+maior = ''
+qtd_maior = 0
+
+while i < len(frase):
+    
+    letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qtd = frase.count(letra_atual)
+
+    if i == 0 or qtd > qtd_maior:
+        maior = letra_atual
+        qtd_maior = qtd
+    
+    i += 1
+
+print(maior, qtd_maior)
+        
+
